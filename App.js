c351524e655d010f84456e1170772ab5e010dc6b
David Maser <davidemaser@gmail.com>
2017-08-21 14:57:30 -0400

    Object indexing

--- App.js
+++ App.js
@@ -2,5 +2,28 @@
  * Created by David Maser on 21/08/2017.
  */
 import Core from './src/base/Perseus';
+import PerseusIndex from './src/base/PerseusIndex';
 
-new Core();
+$.when(
+  new Core(
+    {
+      delay:0,
+      init:true,
+      schema:{
+        body:'body',
+        tag:'pss',
+        exclude:[],
+        async:true,
+        json:{
+          root:'./project/data/',
+          format:'.json',
+          clean:true
+        }
+      }
+    },
+    {},
+    null
+  )
+).then(function(){
+  new PerseusIndex()
+});
---
+++ src/base/PerseusIndex.js
@@ -0,0 +1,11 @@
+/**
+ * Created by David Maser on 21/08/2017.
+ */
+export default class PerseusIndex{
+  constructor(){
+
+  }
+  run(){
+
+  }
+}
